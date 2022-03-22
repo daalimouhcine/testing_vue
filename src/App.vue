38c97d06d0d61dfd4c4f4d0af2eb65da74a634e1
Mouhcine Daali <daalim277@gmail.com>
2022-03-22 10:57:07 +0100

    add form component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app" class="small-container">
-    <h1>Employees</h1>
-
+    <h2>Add Employee</h2>
+    <Employee-input />
+    <h2>Employees</h2>
     <employee-table />
   </div>
 </template>
 
 <script>
   import EmployeeTable from '@/components/EmployeeTable.vue'
+  import EmployeeInput from '@/components/EmployeeInput.vue'
 
   export default {
     name: 'app',
     components: {
       EmployeeTable,
+      EmployeeInput
     },
   }
 </script>
