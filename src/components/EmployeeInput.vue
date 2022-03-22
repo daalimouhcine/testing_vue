3aabfd455e7909f2277eaebbdbd227155ff59209
Mouhcine Daali <daalim277@gmail.com>
2022-03-22 10:57:21 +0100

    create new component for input form

---
+++ src/components/EmployeeInput.vue
@@ -0,0 +1,20 @@
+<template>
+    <form id="Employee-input" action="" method="post">
+        <div>
+            <label for="fullName">Full Name</label>
+            <input type="text" name="fullName">
+        </div>
+        <div>
+            <label for="email">Full Name</label>
+            <input type="email" name="email">
+        </div>
+    </form>
+</template>
+
+<script>
+    export default {
+        name: 'Employee-input',
+    }
+</script>
+
+<style scoped></style>
