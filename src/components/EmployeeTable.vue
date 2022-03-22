662ed70c4d555ca564972b2d75fa60e7a374d555
Mouhcine Daali <daalim277@gmail.com>
2022-03-22 10:43:28 +0100

    add a component

---
+++ src/components/EmployeeTable.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="employee-table">
+    <table>
+      <thead>
+        <tr>
+          <th>Employee name</th>
+          <th>Employee email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Richard Hendricks</td>
+          <td>richard@piedpiper.com</td>
+        </tr>
+        <tr>
+          <td>Bertram Gilfoyle</td>
+          <td>gilfoyle@piedpiper.com</td>
+        </tr>
+        <tr>
+          <td>Dinesh Chugtai</td>
+          <td>dinesh@piedpiper.com</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'employee-table',
+  }
+</script>
+
+<style scoped></style>
